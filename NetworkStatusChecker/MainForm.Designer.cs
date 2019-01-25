f1db2bade031a325fe0c4298dc64daf35c65379d
yeasinmahi <yeasinmahi72@yahoo.com>
2019-01-26 01:06:19 +0600

    Init

---
+++ NetworkStatusChecker/MainForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace NetworkStatusChecker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.BackColor = System.Drawing.Color.Transparent;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.labelStatus.Location = new System.Drawing.Point(12, 29);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(115, 39);
+            this.labelStatus.TabIndex = 0;
+            this.labelStatus.Text = "label1";
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(284, 95);
+            this.Controls.Add(this.labelStatus);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Opacity = 0.9D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Network Status Cheker";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ NetworkStatusChecker/MainForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace NetworkStatusChecker
+{
+    public partial class MainForm : Form
+    {
+        private System.Timers.Timer timer;
+        private int startPosX;
+        private int startPosY;
+        private bool isShown = false;
+        private readonly int holdTimer = 300;
+        private int counter = 0;
+        public MainForm()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        public void Init()
+        {
+            // We want our window to be the top most
+            TopMost = true;
+            // Pop doesn't need to be shown in task bar
+            ShowInTaskbar = false;
+
+        }
+        public void CreateTimer()
+        {
+            // Create and run timer for animation
+            timer = new System.Timers.Timer();
+            timer.Interval = 5;
+            timer.Elapsed += new ElapsedEventHandler(timer_Tick);
+
+            //timer.Tick += timer_Tick;
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            // Move window out of screen
+            startPosX = Screen.PrimaryScreen.WorkingArea.Width - Width;
+            startPosY = Screen.PrimaryScreen.WorkingArea.Height;
+            SetDesktopLocation(startPosX, startPosY);
+            CreateTimer();
+            timer.Enabled = true;
+            base.OnLoad(e);
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //Lift window by 1 pixels
+            if (isShown)
+            {
+                if (counter > holdTimer)
+                {
+                    startPosY += 1;
+                }
+                else
+                {
+                    counter++;
+                }
+            }
+            else
+            {
+                startPosY -= 1;
+            }
+
+            //If window is fully visible stop the timer
+            if (startPosY < Screen.PrimaryScreen.WorkingArea.Height - Height)
+            {
+                isShown = true;
+                //timer.Stop();
+            }
+            else
+            {
+                if (InvokeRequired)
+                {
+                    // after we've done all the processing, 
+                    this.Invoke(new MethodInvoker(delegate
+                    {
+                        SetDesktopLocation(startPosX, startPosY);
+                    }));
+                }
+                else
+                {
+                    SetDesktopLocation(startPosX, startPosY);
+                }
+
+            }
+            if (isShown && startPosY > Screen.PrimaryScreen.WorkingArea.Height + (Height / 2))
+            {
+                isShown = false;
+                counter = 0;
+                timer.Enabled = false;
+            }
+
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            SetNetworkStatus(GetNetworkStatus());
+            NetworkChange.NetworkAvailabilityChanged += new NetworkAvailabilityChangedEventHandler(NetworkChange_NetworkAvailabilityChanged);
+
+        }
+
+        private void NetworkChange_NetworkAvailabilityChanged(object sender, NetworkAvailabilityEventArgs e)
+        {
+            SetNetworkStatus(GetNetworkStatus());
+        }
+
+        private bool GetNetworkStatus()
+        {
+            return NetworkInterface.GetIsNetworkAvailable();
+        }
+
+        private void SetNetworkStatus(bool networkUp)
+        {
+            timer.Enabled = true;
+            if (networkUp)
+            {
+                ThreadHelper.SetText(this, labelStatus, "Network Ok");
+                //labelStatus.Text = "Network Ok";
+            }
+            else
+            {
+                ThreadHelper.SetText(this, labelStatus, "Network Down");
+                //labelStatus.Text = "Network Down";
+            }
+        }
+    }
+}
